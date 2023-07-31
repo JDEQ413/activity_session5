e3d9f65abb122197eaf1cdacc7b6bc186986ce50
usuario <a01688413@tec.mx>
2023-07-30 21:30:15 -0700

    last update pre-commit yaml autopep8 flake8 (ignore) isort autoflake

--- iris.py
+++ iris.py
@@ -1,4 +1,8 @@
+import json  # This is an unused import
+import os  # This is an unused import
+
 import numpy as np  # Import in incorrect order
+from my_library import test  # This is an unused import
 from sklearn.datasets import load_iris  # Import in incorrect order
 from sklearn.linear_model import \
     LogisticRegression  # Import in incorrect order
